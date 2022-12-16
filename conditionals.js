31e2ab33f3efa6edf773032c26082002313d18d4
Nick.Holm <nick416holm@protonmail.com>
2022-12-16 14:24:51 -0500

    checking conditionals to tell peeps about the weather

---
+++ conditionals.js
@@ -0,0 +1,32 @@
+// const raining = true;
+// const cold = true;
+
+// if (raining) {
+//   console.log("Make sure to bring an umbrella!");
+// }
+
+// if (cold) {
+//   console.log("Make sure to wear a scarf!");
+// }
+
+// console.log("Now you're ready to go outside.");
+
+// const cold = false;
+
+// if (cold) {
+//   console.log("Make sure to wear a scarf!");
+// } else {
+//   console.log("Short sleeves are fine.");
+// }
+
+const temp = 12;
+
+if (temp < 0) {
+  console.log("Make sure to wear a scarf!");
+} else if (temp < 15) {
+  console.log("Short sleeves won't cut it.");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+console.log("Now you're ready to go outside.");
