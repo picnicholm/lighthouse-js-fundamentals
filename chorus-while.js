3eb3eaa78af5d6f556ec1a7169d2d4dc598b400d
Nick.Holm <nick416holm@protonmail.com>
2022-12-16 15:57:24 -0500

    for loop version

--- chorus-while.js
+++ chorus-while.js
@@ -1,12 +1,21 @@
-const chorus = "Let's Dance!";
-let repeat = 0;
+// const chorus = "Let's Dance!";
+// let repeat = 0;
 
-while (repeat < 10) {
+// while (repeat < 10) {
+//   if (repeat === 5) {
+//     console.log("*change key*");
+//   }
+//   console.log(chorus);
+//   repeat++;
+// }
+
+const chorus = "Let's Dance!";
+
+for (let repeat = 0; repeat < 10; repeat++) {
   if (repeat === 5) {
     console.log("*change key*");
   }
   console.log(chorus);
-  repeat++;
 }
 
 console.log("Until the sun comes up!");
