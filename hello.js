42fca4a70a2ce6a475a94d4778df3072efa4670f
Nick.Holm <nick416holm@protonmail.com>
2022-12-16 17:08:29 -0500

    log vs return

--- hello.js
+++ hello.js
@@ -6,3 +6,16 @@ sayHello("Nick");
 sayHello("Kaz");
 sayHello("Bonita");
 sayHello("Clarice");
+
+const sayHelloToConsole = function (name) {
+  console.log("Hello, " + name);
+};
+
+sayHelloToConsole("Gilly");
+
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+};
+
+const greeting = returnSayHello("Pink Champagne");
+console.log(greeting);
